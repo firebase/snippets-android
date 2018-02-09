6c92a6f3e3390a11e0d64b018e0344fd50ba213b
the-dagger <harshithdwivedi@gmail.com>
2018-02-10 01:27:38 +0530

    feat : kotlinized database snippet
    Signed-off-by: the-dagger <harshithdwivedi@gmail.com>

---
+++ database/app/src/main/java/com/google/firebase/referencecode/database/interfaces/ActivityInterface.java
@@ -0,0 +1,10 @@
+package com.google.firebase.referencecode.database.interfaces;
+
+/**
+ * An interface to be implemented by the Activity.
+ * Add any new method to this interface instead of adding it directly to the activity
+ */
+
+public interface ActivityInterface {
+    void basicReadWrite();
+}

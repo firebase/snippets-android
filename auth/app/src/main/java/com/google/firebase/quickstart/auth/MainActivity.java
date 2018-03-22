ea1c1c9186cfd78da707628d976f741c6d149ce5
bojeil-google <bojeil-google@users.noreply.github.com>
2018-03-22 09:42:05 -0700

    Updated comment referencing deprecated getToken()
    Replaced deprecated getToken() with getIdToken()

--- auth/app/src/main/java/com/google/firebase/quickstart/auth/MainActivity.java
+++ auth/app/src/main/java/com/google/firebase/quickstart/auth/MainActivity.java
@@ -72,7 +72,7 @@ public class MainActivity extends AppCompatActivity implements MainActivityInter
 
             // The user's ID, unique to the Firebase project. Do NOT use this value to
             // authenticate with your backend server, if you have one. Use
-            // FirebaseUser.getToken() instead.
+            // FirebaseUser.getIdToken() instead.
             String uid = user.getUid();
         }
         // [END get_user_profile]

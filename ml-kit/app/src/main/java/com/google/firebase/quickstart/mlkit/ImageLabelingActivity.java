ff19d3857a43fc6bc8a35146498d13a78e7f8e50
Kevin Cheung <kevincheung@google.com>
2018-05-09 17:24:59 -0700

    Add cloud label detection snippets

--- ml-kit/app/src/main/java/com/google/firebase/quickstart/mlkit/ImageLabelingActivity.java
+++ ml-kit/app/src/main/java/com/google/firebase/quickstart/mlkit/ImageLabelingActivity.java
@@ -8,6 +8,9 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.cloud.FirebaseVisionCloudDetectorOptions;
+import com.google.firebase.ml.vision.cloud.label.FirebaseVisionCloudLabel;
+import com.google.firebase.ml.vision.cloud.label.FirebaseVisionCloudLabelDetector;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.label.FirebaseVisionLabel;
 import com.google.firebase.ml.vision.label.FirebaseVisionLabelDetector;
@@ -38,7 +41,7 @@ public class ImageLabelingActivity extends AppCompatActivity {
 
         /*
         // [START get_detector_options]
-        // Or, to set the minimum confidense required:
+        // Or, to set the minimum confidence required:
         FirebaseVisionLabelDetector detector = FirebaseVision.getInstance()
                 .getVisionLabelDetector(options);
         // [END get_detector_options]
@@ -59,7 +62,7 @@ public class ImageLabelingActivity extends AppCompatActivity {
                                             String entityId = label.getEntityId();
                                             float confidence = label.getConfidence();
                                         }
-                                        // [END run_detector]
+                                        // [END get_labels]
                                         // [END_EXCLUDE]
                                     }
                                 })
@@ -72,5 +75,51 @@ public class ImageLabelingActivity extends AppCompatActivity {
                                     }
                                 });
         // [END run_detector]
+    }
+
+    private void labelImagesCloud(FirebaseVisionImage image) {
+        // [START set_detector_options_cloud]
+        FirebaseVisionCloudDetectorOptions options = new FirebaseVisionCloudDetectorOptions.Builder()
+                .setModelType(FirebaseVisionCloudDetectorOptions.LATEST_MODEL)
+                .setMaxResults(30)
+                .build();
+        // [END set_detector_options_cloud]
+
+        // [START get_detector_cloud]
+        FirebaseVisionCloudLabelDetector detector = FirebaseVision.getInstance()
+                .getVisionCloudLabelDetector();
+        // Or, to change the default settings:
+        // FirebaseVisionCloudLabelDetector detector = FirebaseVision.getInstance()
+        //         .getVisionCloudLabelDetector(options);
+        // [END get_detector_cloud]
+
+        // [START run_detector_cloud]
+        Task<List<FirebaseVisionCloudLabel>> result =
+                detector.detectInImage(image)
+                        .addOnSuccessListener(
+                                new OnSuccessListener<List<FirebaseVisionCloudLabel>>() {
+                                    @Override
+                                    public void onSuccess(List<FirebaseVisionCloudLabel> labels) {
+                                        // Task completed successfully
+                                        // [START_EXCLUDE]
+                                        // [START get_labels_cloud]
+                                        for (FirebaseVisionCloudLabel label: labels) {
+                                            String text = label.getLabel();
+                                            String entityId = label.getEntityId();
+                                            float confidence = label.getConfidence();
+                                        }
+                                        // [END get_labels_cloud]
+                                        // [END_EXCLUDE]
+                                    }
+                                })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        // Task failed with an exception
+                                        // ...
+                                    }
+                                });
+        // [END run_detector_cloud]
     }
 }

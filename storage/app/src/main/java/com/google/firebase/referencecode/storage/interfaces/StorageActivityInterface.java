a7f0ed533e9e87931ca76b853c1ae58564b027d6
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2018-07-13 00:59:30 +0200

    feat: Create Storage Sample in Kotlin - Add Kotlin Support to build.gradle
    - Create interface to keep both java and kotlin Activities in sync
    
    - Update Java Activity to implement the interface and override their methods
    
    - Create Kotlin Activity
    
    - Add Kotlin Activity to AndroidManifest

---
+++ storage/app/src/main/java/com/google/firebase/referencecode/storage/interfaces/StorageActivityInterface.java
@@ -0,0 +1,22 @@
+package com.google.firebase.referencecode.storage.interfaces;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public interface StorageActivityInterface {
+
+    void includesForCreateReference();
+
+    void includesForUploadFiles() throws FileNotFoundException;
+
+    void includesForDownloadFiles() throws IOException;
+
+    void includesForFileMetadata();
+
+    void includesForMetadata_delete();
+
+    void includesForMetadata_custom();
+
+    void includesForDeleteFiles();
+
+}

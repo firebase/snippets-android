47d63ad079456aff8821d0eda5469dcebf0a3afb
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2018-07-13 22:38:46 +0200

    feat: Create Dynamic Links Sample in Kotlin
    - Add Kotlin Support to build.gradle
    
    - Create interface to keep both java and kotlin Activities in sync
    
    - Update Java Activity to implement the interface and override their methods
    
    - Create Kotlin Activity
    
    - Add Kotlin Activity to AndroidManifest
    
    - Correct minor typo on shortenLongLink() from Java Activity

---
+++ dynamic-links/app/src/main/java/com/google/firebase/quickstart/dynamiclinks/interfaces/MainActivityInterface.java
@@ -0,0 +1,12 @@
+package com.google.firebase.quickstart.dynamiclinks.interfaces;
+
+public interface MainActivityInterface {
+
+    void createDynamicLink_Basic();
+
+    void createDynamicLink_Advanced();
+
+    void createShortLink();
+
+    void shortenLongLink();
+}

52cc4a14188793ef19f89020252522c7ef7e273e
Sam Stern <samstern@google.com>
2018-08-20 15:51:41 -0700

    Phone auth testing snippets
    Change-Id: I516b29c7425802fb97e0e1629d0d045eb64be4d0

--- auth/app/src/main/java/com/google/firebase/quickstart/auth/MainActivity.java
+++ auth/app/src/main/java/com/google/firebase/quickstart/auth/MainActivity.java
@@ -26,21 +26,26 @@ import android.widget.Toast;
 import com.facebook.AccessToken;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.ActionCodeSettings;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthSettings;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GithubAuthProvider;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.firebase.auth.SignInMethodQueryResult;
 import com.google.firebase.auth.UserInfo;
 import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.quickstart.auth.interfaces.MainActivityInterface;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity implements MainActivityInterface {
 
@@ -513,4 +518,79 @@ public class MainActivity extends AppCompatActivity implements MainActivityInter
         FirebaseAuth.getInstance().signOut();
     }
 
+    // TODO: kotlin
+    public void testPhoneVerify() {
+        // [START auth_test_phone_verify]
+        String phoneNum = "+16505554567";
+        String testVerificationCode = "123456";
+
+        // Whenever verification is triggered with the whitelisted number,
+        // provided it is not set for auto-retrieval, onCodeSent will be triggered.
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phoneNum, 30L /*timeout*/, TimeUnit.SECONDS,
+                this, new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+                    @Override
+                    public void onCodeSent(String verificationId,
+                                           PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                        // Save the verification id somewhere
+                        // ...
+
+                        // The corresponding whitelisted code above should be used to complete sign-in.
+                        MainActivity.this.enableUserManuallyInputCode();
+                    }
+
+                    @Override
+                    public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+                        // Sign in with the credential
+                        // ...
+                    }
+
+                    @Override
+                    public void onVerificationFailed(FirebaseException e) {
+                         // ...
+                    }
+
+                });
+        // [END auth_test_phone_verify]
+    }
+
+    private void enableUserManuallyInputCode() {
+        // No-op
+    }
+
+    public void testPhoneAutoRetrieve() {
+        // [START auth_test_phone_auto]
+        // The test phone number and code should be whitelisted in the console.
+        String phoneNumber = "+16505554567";
+        String smsCode = "123456";
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseAuthSettings firebaseAuthSettings = firebaseAuth.getFirebaseAuthSettings();
+
+        // Configure faking the auto-retrieval with the whitelisted numbers.
+        firebaseAuthSettings.setAutoRetrievedSmsCodeForPhoneNumber(phoneNumber, smsCode);
+
+        PhoneAuthProvider phoneAuthProvider = PhoneAuthProvider.getInstance();
+        phoneAuthProvider.verifyPhoneNumber(
+                phoneNumber,
+                60L,
+                TimeUnit.SECONDS,
+                this, /* activity */
+                new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    @Override
+                    public void onVerificationCompleted(PhoneAuthCredential credential) {
+                        // Instant verification is applied and a credential is directly returned.
+                        // ...
+                    }
+
+                    // [START_EXCLUDE]
+                    @Override
+                    public void onVerificationFailed(FirebaseException e) {
+
+                    }
+                    // [END_EXCLUDE]
+                });
+        // [END auth_test_phone_auto]
+    }
 }

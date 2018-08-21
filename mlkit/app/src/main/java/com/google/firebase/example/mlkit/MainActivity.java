f805f491e1bb97da15c450031c158946dfac98f2
Sam Stern <samstern@google.com>
2018-08-21 15:14:41 -0700

    Add MLKit snippet
    Change-Id: I03c6ec4860d2e278bdd3cbb2f3cca597b307650d

--- mlkit/app/src/main/java/com/google/firebase/example/mlkit/MainActivity.java
+++ mlkit/app/src/main/java/com/google/firebase/example/mlkit/MainActivity.java
@@ -3,11 +3,23 @@ package com.google.firebase.example.mlkit;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.firebase.ml.vision.cloud.FirebaseVisionCloudDetectorOptions;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    public void buildCloudVisionOptions() {
+        // [START ml_build_cloud_vision_options]
+        FirebaseVisionCloudDetectorOptions options =
+                new FirebaseVisionCloudDetectorOptions.Builder()
+                        .setModelType(FirebaseVisionCloudDetectorOptions.LATEST_MODEL)
+                        .setMaxResults(15)
+                        .build();
+        // [END ml_build_cloud_vision_options]
     }
 
 }

8c22c8ab63dba46ea64719d72b77efd2f32f8374
the-dagger <harshithdwivedi@gmail.com>
2018-09-03 17:21:07 +0530

    feat : kotlinize crash snippets

---
+++ crash/app/src/main/java/com/google/firebase/example/crash/interfaces/MainActivityInterface.kt
@@ -0,0 +1,12 @@
+package com.google.firebase.example.crash.interfaces
+
+/**
+ * An interface to be implemented by the Activity.
+ * Add any new method to this interface instead of adding it directly to the activity
+ */
+
+interface MainActivityInterface {
+    fun logSimple()
+    fun disableCollection()
+    fun enableCollection()
+}

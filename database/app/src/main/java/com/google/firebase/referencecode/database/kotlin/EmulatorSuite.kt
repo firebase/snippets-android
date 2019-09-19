90e205c8b1a72c862095ad04f028afd3bfe00d15
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2019-09-19 21:46:07 +0200

    Fix the emulator snippets and add its kotlin equivalent (#135)

--- database/app/src/main/java/com/google/firebase/referencecode/database/kotlin/EmulatorSuite.kt
+++ database/app/src/main/java/com/google/firebase/referencecode/database/kotlin/EmulatorSuite.kt
@@ -1,24 +1,22 @@
-package com.google.firebase.referencecode.database;
+package com.google.firebase.referencecode.database.kotlin
 
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.FirebaseDatabase
 
-public class EmulatorSuite {
+class EmulatorSuite {
 
-    public void emulatorSettings() {
+    fun emulatorSettings() {
         // [START rtdb_emulator_connect]
         // 10.0.2.2 is the special IP address to connect to the 'localhost' of
         // the host computer from an Android emulator.
         // In almost all cases the ns (namespace) is your project ID.
-        FirebaseDatabase database = FirebaseDatabase.getInstance("http://10.0.2.2:9000?ns=YOUR_DATABASE_NAMESPACE");
+        val database = FirebaseDatabase.getInstance("http://10.0.2.2:9000?ns=YOUR_DATABASE_NAMESPACE")
         // [END rtdb_emulator_connect]
     }
 
-    public void flushRealtimeDatabase(FirebaseDatabase database) {
+    fun flushRealtimeDatabase(database: FirebaseDatabase) {
         // [START rtdb_emulator_flush]
         // With a DatabaseReference, write null to clear the database.
-        database.getReference().setValue(null);
+        database.reference.setValue(null)
         // [END rtdb_emulator_flush]
-
     }
-
 }

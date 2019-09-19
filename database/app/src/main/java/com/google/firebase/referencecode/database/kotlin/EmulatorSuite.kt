0f3ecb5dadfa690f5a50866da5acdba3808f95dd
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2019-09-19 21:33:46 +0200

    feat: add EmulatorSuite's Kotlin equivalent class

---
+++ database/app/src/main/java/com/google/firebase/referencecode/database/kotlin/EmulatorSuite.kt
@@ -0,0 +1,22 @@
+package com.google.firebase.referencecode.database.kotlin
+
+import com.google.firebase.database.FirebaseDatabase
+
+class EmulatorSuite {
+
+    fun emulatorSettings() {
+        // [START rtdb_emulator_connect]
+        // 10.0.2.2 is the special IP address to connect to the 'localhost' of
+        // the host computer from an Android emulator.
+        // In almost all cases the ns (namespace) is your project ID.
+        val database = FirebaseDatabase.getInstance("http://10.0.2.2:9000?ns=YOUR_DATABASE_NAMESPACE")
+        // [END rtdb_emulator_connect]
+    }
+
+    fun flushRealtimeDatabase(database: FirebaseDatabase) {
+        // [START rtdb_emulator_flush]
+        // With a DatabaseReference, write null to clear the database.
+        database.reference.setValue(null)
+        // [END rtdb_emulator_flush]
+    }
+}

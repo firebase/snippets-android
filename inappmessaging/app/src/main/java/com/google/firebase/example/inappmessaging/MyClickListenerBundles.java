71e14fbcfd14668c480fc94bf74268e1a9eaf1cc
Mark Arndt <markarndt@google.com>
2020-01-16 15:05:38 -0800

    Fix up. gradlw ktlint had no trouble with these.

--- inappmessaging/app/src/main/java/com/google/firebase/example/inappmessaging/MyClickListenerBundles.java
+++ inappmessaging/app/src/main/java/com/google/firebase/example/inappmessaging/MyClickListenerBundles.java
@@ -1,24 +1,25 @@
 package com.google.firebase.example.inappmessaging;
 
+import java.util.Map;
+
 import com.google.firebase.inappmessaging.FirebaseInAppMessagingClickListener;
 import com.google.firebase.inappmessaging.model.Action;
 import com.google.firebase.inappmessaging.model.CampaignMetadata;
 import com.google.firebase.inappmessaging.model.InAppMessage;
 
-// [START fiam_click_listener]
-public class MyClickListener implements FirebaseInAppMessagingClickListener {
+// [START fiam_click_listener_bundles]
+public class MyClickListenerBundles implements FirebaseInAppMessagingClickListener {
 
     @Override
     public void messageClicked(InAppMessage inAppMessage, Action action) {
         // Determine which URL the user clicked
         String url = action.getActionUrl();
 
-        // Get general information about the campaign
-        CampaignMetadata metadata = inAppMessage.getCampaignMetadata();
+        // Get data bundle for the inapp message
+        Map<String, String> dataBundle = inAppMessage.getData();
 
         // ...
     }
 
 }
-// [END fiam_click_listener]
-
+// [END fiam_click_listener_bundles]
--- inappmessaging/app/src/main/java/com/google/firebase/example/inappmessaging/kotlin/MyClickListenerBundles.kt
+++ inappmessaging/app/src/main/java/com/google/firebase/example/inappmessaging/kotlin/MyClickListenerBundles.kt
@@ -4,17 +4,16 @@ import com.google.firebase.inappmessaging.FirebaseInAppMessagingClickListener
 import com.google.firebase.inappmessaging.model.Action
 import com.google.firebase.inappmessaging.model.InAppMessage
 
-// [START fiam_click_listener]
-class MyClickListener : FirebaseInAppMessagingClickListener {
+class MyClickListenerBundles : FirebaseInAppMessagingClickListener {
 
+// [START fiam_click_listener_bundles]
     override fun messageClicked(inAppMessage: InAppMessage, action: Action) {
         // Determine which URL the user clicked
         val url = action.actionUrl
 
-        // Get general information about the campaign
-        val metadata = inAppMessage.campaignMetadata
-
+        // Get data bundle for the inapp message
+        val dataBundle = inAppMessage.getData()
         // ...
     }
+    // [END fiam_click_listener_bundles]
 }
-// [END fiam_click_listener]

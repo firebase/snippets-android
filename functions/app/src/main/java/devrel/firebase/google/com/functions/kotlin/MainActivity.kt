7d066513d560eb2c721eaf5d5bfe341855a4f6ee
Sam Stern <samstern@google.com>
2020-02-10 14:56:36 -0800

    Functions + KTX
    Change-Id: I596f2e19e547f6cc19d22b429c914c6a5f08e2b4

--- functions/app/src/main/java/devrel/firebase/google/com/functions/kotlin/MainActivity.kt
+++ functions/app/src/main/java/devrel/firebase/google/com/functions/kotlin/MainActivity.kt
@@ -1,13 +1,14 @@
 package devrel.firebase.google.com.functions.kotlin
 
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.functions.FirebaseFunctions
+import com.google.firebase.functions.ktx.functions
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     fun emulatorSettings() {
         // [START functions_emulator_connect]
-        FirebaseFunctions.getInstance().useFunctionsEmulator("http://10.0.2.2:5001")
+        Firebase.functions.useFunctionsEmulator("http://10.0.2.2:5001")
         // [END functions_emulator_connect]
     }
 }

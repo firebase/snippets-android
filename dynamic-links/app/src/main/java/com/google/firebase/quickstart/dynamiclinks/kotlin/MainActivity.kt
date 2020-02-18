854e0ffcad41a7ba699a43742cd5a90528a10602
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2020-02-18 19:10:39 +0200

    Dynamic Links + KTX (#177)

--- dynamic-links/app/src/main/java/com/google/firebase/quickstart/dynamiclinks/kotlin/MainActivity.kt
+++ dynamic-links/app/src/main/java/com/google/firebase/quickstart/dynamiclinks/kotlin/MainActivity.kt
@@ -6,9 +6,16 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.appinvite.AppInviteReferral
 import com.google.firebase.appinvite.FirebaseAppInvite
-import com.google.firebase.dynamiclinks.DynamicLink
-import com.google.firebase.dynamiclinks.FirebaseDynamicLinks
 import com.google.firebase.dynamiclinks.ShortDynamicLink
+import com.google.firebase.dynamiclinks.ktx.androidParameters
+import com.google.firebase.dynamiclinks.ktx.dynamicLink
+import com.google.firebase.dynamiclinks.ktx.dynamicLinks
+import com.google.firebase.dynamiclinks.ktx.googleAnalyticsParameters
+import com.google.firebase.dynamiclinks.ktx.iosParameters
+import com.google.firebase.dynamiclinks.ktx.itunesConnectAnalyticsParameters
+import com.google.firebase.dynamiclinks.ktx.shortLinkAsync
+import com.google.firebase.dynamiclinks.ktx.socialMetaTagParameters
+import com.google.firebase.ktx.Firebase
 import com.google.firebase.quickstart.dynamiclinks.R
 
 abstract class MainActivity : AppCompatActivity() {
@@ -20,14 +27,14 @@ abstract class MainActivity : AppCompatActivity() {
 
     fun createDynamicLink_Basic() {
         // [START create_link_basic]
-        val dynamicLink = FirebaseDynamicLinks.getInstance().createDynamicLink()
-                .setLink(Uri.parse("https://www.example.com/"))
-                .setDomainUriPrefix("https://example.page.link")
-                // Open links with this app on Android
-                .setAndroidParameters(DynamicLink.AndroidParameters.Builder().build())
-                // Open links with com.example.ios on iOS
-                .setIosParameters(DynamicLink.IosParameters.Builder("com.example.ios").build())
-                .buildDynamicLink()
+        val dynamicLink = Firebase.dynamicLinks.dynamicLink {
+            link = Uri.parse("https://www.example.com/")
+            domainUriPrefix = "https://example.page.link"
+            // Open links with this app on Android
+            androidParameters { }
+            // Open links with com.example.ios on iOS
+            iosParameters("com.example.ios") { }
+        }
 
         val dynamicLinkUri = dynamicLink.uri
         // [END create_link_basic]
@@ -35,81 +42,73 @@ abstract class MainActivity : AppCompatActivity() {
 
     fun createDynamicLink_Advanced() {
         // [START create_link_advanced]
-        val dynamicLink = FirebaseDynamicLinks.getInstance().createDynamicLink()
-                .setLink(Uri.parse("https://www.example.com/"))
-                .setDomainUriPrefix("https://example.page.link")
-                .setAndroidParameters(
-                        DynamicLink.AndroidParameters.Builder("com.example.android")
-                                .setMinimumVersion(125)
-                                .build())
-                .setIosParameters(
-                        DynamicLink.IosParameters.Builder("com.example.ios")
-                                .setAppStoreId("123456789")
-                                .setMinimumVersion("1.0.1")
-                                .build())
-                .setGoogleAnalyticsParameters(
-                        DynamicLink.GoogleAnalyticsParameters.Builder()
-                                .setSource("orkut")
-                                .setMedium("social")
-                                .setCampaign("example-promo")
-                                .build())
-                .setItunesConnectAnalyticsParameters(
-                        DynamicLink.ItunesConnectAnalyticsParameters.Builder()
-                                .setProviderToken("123456")
-                                .setCampaignToken("example-promo")
-                                .build())
-                .setSocialMetaTagParameters(
-                        DynamicLink.SocialMetaTagParameters.Builder()
-                                .setTitle("Example of a Dynamic Link")
-                                .setDescription("This link works whether the app is installed or not!")
-                                .build())
-                .buildDynamicLink() // Or buildShortDynamicLink()
+        val dynamicLink = Firebase.dynamicLinks.dynamicLink { // or Firebase.dynamicLinks.shortLinkAsync
+            link = Uri.parse("https://www.example.com/")
+            domainUriPrefix = "https://example.page.link"
+            androidParameters("com.example.android") {
+                minimumVersion = 125
+            }
+            iosParameters("com.example.ios") {
+                appStoreId = "123456789"
+                minimumVersion = "1.0.1"
+            }
+            googleAnalyticsParameters {
+                source = "orkut"
+                medium = "social"
+                campaign = "example-promo"
+            }
+            itunesConnectAnalyticsParameters {
+                providerToken = "123456"
+                campaignToken = "example-promo"
+            }
+            socialMetaTagParameters {
+                title = "Example of a Dynamic Link"
+                description = "This link works whether the app is installed or not!"
+            }
+        }
         // [END create_link_advanced]
     }
 
     fun createShortLink() {
         // [START create_short_link]
-        val shortLinkTask = FirebaseDynamicLinks.getInstance().createDynamicLink()
-                .setLink(Uri.parse("https://www.example.com/"))
-                .setDomainUriPrefix("https://example.page.link")
-                // Set parameters
-                // ...
-                .buildShortDynamicLink()
-                .addOnSuccessListener { result ->
-                    // Short link created
-                    val shortLink = result.shortLink
-                    val flowchartLink = result.previewLink
-                }.addOnFailureListener {
-                    // Error
-                    // ...
-                }
+        val shortLinkTask = Firebase.dynamicLinks.shortLinkAsync {
+            link = Uri.parse("https://www.example.com/")
+            domainUriPrefix = "https://example.page.link"
+            // Set parameters
+            // ...
+        }.addOnSuccessListener { result ->
+            // Short link created
+            val shortLink = result.shortLink
+            val flowchartLink = result.previewLink
+        }.addOnFailureListener {
+            // Error
+            // ...
+        }
         // [END create_short_link]
     }
 
     fun shortenLongLink() {
         // [START shorten_long_link]
-        val shortLinkTask = FirebaseDynamicLinks.getInstance().createDynamicLink()
-                .setLongLink(Uri.parse("https://example.page.link/?link=" +
-                        "https://www.example.com/&apn=com.example.android&ibn=com.example.ios"))
-                .buildShortDynamicLink()
-                .addOnSuccessListener { result ->
-                    // Short link created
-                    val shortLink = result.shortLink
-                    val flowchartLink = result.previewLink
-                }
-                .addOnFailureListener {
-                    // Error
-                    // ...
-                }
+        val shortLinkTask = Firebase.dynamicLinks.shortLinkAsync {
+            longLink = Uri.parse("https://example.page.link/?link=" +
+                    "https://www.example.com/&apn=com.example.android&ibn=com.example.ios")
+        }.addOnSuccessListener { result ->
+            // Short link created
+            val shortLink = result.shortLink
+            val flowchartLink = result.previewLink
+        }.addOnFailureListener {
+            // Error
+            // ...
+        }
         // [END shorten_long_link]
     }
 
     fun buildShortSuffix() {
         // [START ddl_short_suffix]
-        val shortLinkTask = FirebaseDynamicLinks.getInstance().createDynamicLink()
-                // ...
-                .buildShortDynamicLink(ShortDynamicLink.Suffix.SHORT)
-                // ...
+        val shortLinkTask = Firebase.dynamicLinks.shortLinkAsync(ShortDynamicLink.Suffix.SHORT) {
+            // Set parameters
+            // ...
+        }
         // [END ddl_short_suffix]
     }
 
@@ -126,7 +125,7 @@ abstract class MainActivity : AppCompatActivity() {
 
     fun getInvitation() {
         // [START ddl_get_invitation]
-        FirebaseDynamicLinks.getInstance()
+        Firebase.dynamicLinks
                 .getDynamicLink(intent)
                 .addOnCompleteListener { task ->
                     if (!task.isSuccessful) {

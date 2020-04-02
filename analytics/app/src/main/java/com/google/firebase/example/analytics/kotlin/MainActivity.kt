b1b8e72c311c718c58e4c2548c56a5e6ba7648f4
Sam <samstern@google.com>
2020-04-02 12:48:49 -0400

    Add analytics module

---
+++ analytics/app/src/main/java/com/google/firebase/example/analytics/kotlin/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.google.firebase.example.analytics.kotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.example.analytics.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

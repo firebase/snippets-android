6d5b0ed89f1730cd98758c0583cce8c44da10dd8
Sam Stern <samstern@google.com>
2020-05-11 11:02:31 -0400

    Move to new Crashlytics SDK (#196)

--- crashlytics/app/src/main/java/com/google/firebase/example/crashlytics/kotlin/MainActivity.kt
+++ crashlytics/app/src/main/java/com/google/firebase/example/crashlytics/kotlin/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.google.firebase.example.crashlytics.kotlin
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import android.util.Log
 import android.view.ViewGroup
 import android.widget.Button
-import com.crashlytics.android.Crashlytics
-import io.fabric.sdk.android.Fabric
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,83 +12,76 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
     }
 
-    private fun setKeysBasic(key: String) {
+    fun setKeysBasic(key: String) {
         // [START crash_set_keys_basic]
-        Crashlytics.setString(key, "foo" /* string value */)
+        val crashlytics = FirebaseCrashlytics.getInstance()
 
-        Crashlytics.setBool(key, true /* boolean value */)
+        crashlytics.setCustomKey(key, "foo" /* string value */)
 
-        Crashlytics.setDouble(key, 1.0 /* double value */)
+        crashlytics.setCustomKey(key, true /* boolean value */)
 
-        Crashlytics.setFloat(key, 1.0f /* float value */)
+        crashlytics.setCustomKey(key, 1.0 /* double value */)
 
-        Crashlytics.setInt(key, 1 /* int value */)
+        crashlytics.setCustomKey(key, 1.0f /* float value */)
+
+        crashlytics.setCustomKey(key, 1 /* int value */)
         // [END crash_set_keys_basic]
     }
 
-    private fun resetKey() {
+    fun resetKey() {
         // [START crash_re_set_key]
-        Crashlytics.setInt("current_level", 3)
-        Crashlytics.setString("last_UI_action", "logged_in")
+        val crashlytics = FirebaseCrashlytics.getInstance()
+        crashlytics.setCustomKey("current_level", 3)
+        crashlytics.setCustomKey("last_UI_action", "logged_in")
         // [END crash_re_set_key]
     }
 
-    private fun logReportAndPrint() {
+    fun logReportAndPrint() {
         // [START crash_log_report_and_print]
-        Crashlytics.log(Log.DEBUG, "tag", "message")
+        FirebaseCrashlytics.getInstance().log("message")
         // [END crash_log_report_and_print]
     }
 
-    private fun logReportOnly() {
+    fun logReportOnly() {
         // [START crash_log_report_only]
-        Crashlytics.log("message")
+        FirebaseCrashlytics.getInstance().log("message")
         // [END crash_log_report_only]
     }
 
-    private fun enableAtRuntime() {
+    fun enableAtRuntime() {
         // [START crash_enable_at_runtime]
-        Fabric.with(this, Crashlytics())
+        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(true)
         // [END crash_enable_at_runtime]
     }
 
-    private fun setUserId() {
+    fun setUserId() {
         // [START crash_set_user_id]
-        Crashlytics.setUserIdentifier("user123456789")
+        FirebaseCrashlytics.getInstance().setUserId("user123456789")
         // [END crash_set_user_id]
     }
 
     @Throws(Exception::class)
-    private fun methodThatThrows() {
+    fun methodThatThrows() {
         throw Exception()
     }
 
-    private fun logCaughtEx() {
+    fun logCaughtEx() {
         // [START crash_log_caught_ex]
         try {
             methodThatThrows()
         } catch (e: Exception) {
-            Crashlytics.logException(e)
+            FirebaseCrashlytics.getInstance().recordException(e)
             // handle your exception here
         }
         // [END crash_log_caught_ex]
     }
 
-    private fun enableDebugMode() {
-        // [START crash_enable_debug_mode]
-        val fabric = Fabric.Builder(this)
-                .kits(Crashlytics())
-                .debuggable(true) // Enables Crashlytics debugger
-                .build()
-        Fabric.with(fabric)
-        // [END crash_enable_debug_mode]
-    }
-
-    private fun forceACrash() {
+    fun forceACrash() {
         // [START crash_force_crash]
         val crashButton = Button(this)
         crashButton.text = "Crash!"
         crashButton.setOnClickListener {
-            Crashlytics.getInstance().crash() // Force a crash
+            throw RuntimeException() // Force a crash
         }
 
         addContentView(crashButton, ViewGroup.LayoutParams(

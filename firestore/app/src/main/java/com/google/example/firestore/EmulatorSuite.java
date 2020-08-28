0cb18a4bbb9328ca318d1e1e59fde4fd8cc64282
Sam Stern <samstern@google.com>
2020-08-28 06:51:08 -0400

    Move to useEmulator API (#222)

--- firestore/app/src/main/java/com/google/example/firestore/EmulatorSuite.java
+++ firestore/app/src/main/java/com/google/example/firestore/EmulatorSuite.java
@@ -9,13 +9,12 @@ public class EmulatorSuite {
         // [START fs_emulator_connect]
         // 10.0.2.2 is the special IP address to connect to the 'localhost' of
         // the host computer from an Android emulator.
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        firestore.useEmulator("10.0.2.2", 8080);
+
         FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
-                .setHost("10.0.2.2:8080")
-                .setSslEnabled(false)
                 .setPersistenceEnabled(false)
                 .build();
-
-        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
         firestore.setFirestoreSettings(settings);
         // [END fs_emulator_connect]
     }

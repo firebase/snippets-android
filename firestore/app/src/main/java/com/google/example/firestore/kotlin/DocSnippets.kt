985f723931f2a0c437cb222c458128750e95b8cf
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2020-10-22 18:51:29 +0200

    refactor(firestore): add JvmField annotation to kotlin POJO snippet (#243)

--- firestore/app/src/main/java/com/google/example/firestore/kotlin/DocSnippets.kt
+++ firestore/app/src/main/java/com/google/example/firestore/kotlin/DocSnippets.kt
@@ -234,6 +234,7 @@ abstract class DocSnippets(val db: FirebaseFirestore) {
         val name: String? = null,
         val state: String? = null,
         val country: String? = null,
+        @field:JvmField // use this annotation if your Boolean field is prefixed with 'is'
         val isCapital: Boolean? = null,
         val population: Long? = null,
         val regions: List<String>? = null

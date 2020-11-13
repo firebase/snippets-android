b251a1e523e9e9e63b4289972f9db5ed0c3fbd8c
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2020-11-13 12:30:22 +0200

    feat(fcm): use Firebase KTX on the kotlin snippets (#257)

--- messaging/app/src/main/java/com/google/firebase/example/messaging/kotlin/MainActivity.kt
+++ messaging/app/src/main/java/com/google/firebase/example/messaging/kotlin/MainActivity.kt
@@ -1,20 +1,9 @@
 package com.google.firebase.example.messaging.kotlin
 
-import android.accounts.Account
-import android.accounts.AccountManager
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
-import android.util.Log
-import com.google.android.gms.auth.GoogleAuthUtil
-import com.google.firebase.messaging.FirebaseMessaging
-import com.google.firebase.messaging.RemoteMessage
-import org.json.JSONArray
-import org.json.JSONException
-import org.json.JSONObject
-import java.io.IOException
-import java.net.HttpURLConnection
-import java.net.URL
-import java.util.Scanner
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.ktx.messaging
+import com.google.firebase.messaging.ktx.remoteMessage
 import java.util.concurrent.atomic.AtomicInteger
 
 class MainActivity : AppCompatActivity() {
@@ -25,7 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     fun runtimeEnableAutoInit() {
         // [START fcm_runtime_enable_auto_init]
-        FirebaseMessaging.getInstance().isAutoInitEnabled = true
+        Firebase.messaging.isAutoInitEnabled = true
         // [END fcm_runtime_enable_auto_init]
     }
 
@@ -33,10 +22,10 @@ class MainActivity : AppCompatActivity() {
         // [START fcm_device_group_upstream]
         val to = "a_unique_key" // the notification key
         val msgId = AtomicInteger()
-        FirebaseMessaging.getInstance().send(RemoteMessage.Builder(to)
-                .setMessageId(msgId.get().toString())
-                .addData("hello", "world")
-                .build())
+        Firebase.messaging.send(remoteMessage(to) {
+            setMessageId(msgId.get().toString())
+            addData("hello", "world")
+        })
         // [END fcm_device_group_upstream]
     }
 
@@ -44,12 +33,12 @@ class MainActivity : AppCompatActivity() {
         val SENDER_ID = "YOUR_SENDER_ID"
         val messageId = 0 // Increment for each
         // [START fcm_send_upstream]
-        val fm = FirebaseMessaging.getInstance()
-        fm.send(RemoteMessage.Builder("$SENDER_ID@fcm.googleapis.com")
-                .setMessageId(Integer.toString(messageId))
-                .addData("my_message", "Hello World")
-                .addData("my_action", "SAY_HELLO")
-                .build())
+        val fm = Firebase.messaging
+        fm.send(remoteMessage("$SENDER_ID@fcm.googleapis.com") {
+            setMessageId(messageId.toString())
+            addData("my_message", "Hello World")
+            addData("my_action", "SAY_HELLO")
+        })
         // [END fcm_send_upstream]
     }
 }

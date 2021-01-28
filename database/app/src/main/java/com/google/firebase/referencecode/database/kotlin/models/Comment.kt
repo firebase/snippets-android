a94758d5a06f29b487d090dc40bbeb2213ab2f15
Rosário P. Fernandes <rosariofernandes51@gmail.com>
2021-01-28 18:07:26 +0200

    refactor(rtdb): add snippets to QueryActivity from quickstart-android

---
+++ database/app/src/main/java/com/google/firebase/referencecode/database/kotlin/models/Comment.kt
@@ -0,0 +1,7 @@
+package com.google.firebase.referencecode.database.kotlin.models
+
+data class Comment (
+        var uid: String? = "",
+        var author: String? = "",
+        var text: String? = ""
+)
---
+++ database/app/src/main/java/com/google/firebase/referencecode/database/models/Comment.java
@@ -0,0 +1,22 @@
+package com.google.firebase.referencecode.database.models;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class Comment {
+
+    public String uid;
+    public String author;
+    public String text;
+
+    public Comment() {
+        // Default constructor required for calls to DataSnapshot.getValue(Comment.class)
+    }
+
+    public Comment(String uid, String author, String text) {
+        this.uid = uid;
+        this.author = author;
+        this.text = text;
+    }
+
+}

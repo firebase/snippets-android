d017a0c08b01751e8bf814a4f57b80765c045d93
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2021-01-29 16:37:09 +0200

    refactor(rtdb): add snippets from quickstart-android (#275)

--- database/app/src/main/java/com/google/firebase/referencecode/database/kotlin/QueryActivity.kt
+++ database/app/src/main/java/com/google/firebase/referencecode/database/kotlin/QueryActivity.kt
@@ -2,6 +2,7 @@ package com.google.firebase.referencecode.database.kotlin
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
@@ -13,6 +14,7 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.referencecode.database.R
+import com.google.firebase.referencecode.database.kotlin.models.Comment
 import com.google.firebase.referencecode.database.models.Message
 
 /**
@@ -150,6 +152,69 @@ abstract class QueryActivity : AppCompatActivity() {
             // [END_EXCLUDE]
         })
         // [END rtdb_order_by_nested]
+    }
+
+    private fun childEventListenerRecycler() {
+        val context = this
+        // [START child_event_listener_recycler]
+        val childEventListener = object : ChildEventListener {
+            override fun onChildAdded(dataSnapshot: DataSnapshot, previousChildName: String?) {
+                Log.d(TAG, "onChildAdded:" + dataSnapshot.key!!)
+
+                // A new comment has been added, add it to the displayed list
+                val comment = dataSnapshot.getValue<Comment>()
+
+                // ...
+            }
+
+            override fun onChildChanged(dataSnapshot: DataSnapshot, previousChildName: String?) {
+                Log.d(TAG, "onChildChanged: ${dataSnapshot.key}")
+
+                // A comment has changed, use the key to determine if we are displaying this
+                // comment and if so displayed the changed comment.
+                val newComment = dataSnapshot.getValue<Comment>()
+                val commentKey = dataSnapshot.key
+
+                // ...
+            }
+
+            override fun onChildRemoved(dataSnapshot: DataSnapshot) {
+                Log.d(TAG, "onChildRemoved:" + dataSnapshot.key!!)
+
+                // A comment has changed, use the key to determine if we are displaying this
+                // comment and if so remove it.
+                val commentKey = dataSnapshot.key
+
+                // ...
+            }
+
+            override fun onChildMoved(dataSnapshot: DataSnapshot, previousChildName: String?) {
+                Log.d(TAG, "onChildMoved:" + dataSnapshot.key!!)
+
+                // A comment has changed position, use the key to determine if we are
+                // displaying this comment and if so move it.
+                val movedComment = dataSnapshot.getValue<Comment>()
+                val commentKey = dataSnapshot.key
+
+                // ...
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w(TAG, "postComments:onCancelled", databaseError.toException())
+                Toast.makeText(context, "Failed to load comments.",
+                        Toast.LENGTH_SHORT).show()
+            }
+        }
+        databaseReference.addChildEventListener(childEventListener)
+        // [END child_event_listener_recycler]
+    }
+
+    private fun recentPostsQuery() {
+        // [START recent_posts_query]
+        // Last 100 posts, these are automatically the 100 most recent
+        // due to sorting by push() keys.
+        databaseReference.child("posts").limitToFirst(100)
+        // [END recent_posts_query]
     }
 
     override fun onStart() {

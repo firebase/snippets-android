151725f55e4a8d33d12b9ce63a1f0f817dd25c1b
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2021-04-13 12:00:46 +0200

    refactor(admob): delete unused snippets (#291)
    Co-authored-by: DPE bot <dpebot@google.com>

--- admob/app/src/main/java/com/google/firebase/example/admob/MainActivity.java
+++ admob/app/src/main/java/com/google/firebase/example/admob/MainActivity.java
@@ -1,30 +1,12 @@
 package com.google.firebase.example.admob;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.ads.AdListener;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.InterstitialAd;
-import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
 
-import devrel.firebase.google.com.firebaseoptions.R;
-
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "MainActivity";
-
-    private AdView mAdView;
-    private InterstitialAd mInterstitialAd;
-    private Button mLoadInterstitialButton;
-    private Context context = this;
 
     // [START ads_on_create]
     @Override
@@ -34,112 +16,5 @@ public class MainActivity extends AppCompatActivity {
         MobileAds.initialize(this);
     }
     // [END ads_on_create]
-
-    private void loadBannerAd() {
-        // [SNIPPET load_banner_ad]
-        // Load an ad into the AdView.
-        // [START load_banner_ad]
-
-        // Initialize the Google Mobile Ads SDK
-        MobileAds.initialize(context);
-
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
-        // [END load_banner_ad]
-    }
-
-    private void initInterstitialAd() {
-        // [START instantiate_interstitial_ad]
-        // Create an InterstitialAd object. This same object can be re-used whenever you want to
-        // show an interstitial.
-        mInterstitialAd = new InterstitialAd(context);
-        mInterstitialAd.setAdUnitId(getString(R.string.interstitial_ad_unit_id));
-        // [END instantiate_interstitial_ad]
-    }
-
-    private void createInterstitialAdListener() {
-        // [START create_interstitial_ad_listener]
-        mInterstitialAd.setAdListener(new AdListener() {
-            @Override
-            public void onAdClosed() {
-                requestNewInterstitial();
-                beginSecondActivity();
-            }
-
-            @Override
-            public void onAdLoaded() {
-                // Ad received, ready to display
-                // ...
-            }
-
-            @Override
-            public void onAdFailedToLoad(LoadAdError loadAdError) {
-                Log.w(TAG, "onAdFailedToLoad:" + loadAdError.getMessage());
-            }
-        });
-        // [END create_interstitial_ad_listener]
-    }
-
-    private void displayInterstitialAd() {
-        // [START display_interstitial_ad]
-        mLoadInterstitialButton = findViewById(R.id.loadInterstitialButton);
-        mLoadInterstitialButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mInterstitialAd.isLoaded()) {
-                    mInterstitialAd.show();
-                } else {
-                    beginSecondActivity();
-                }
-            }
-        });
-        // [END display_interstitial_ad]
-    }
-
-    /**
-     * Load a new interstitial ad asynchronously.
-     */
-    // [START request_new_interstitial]
-    private void requestNewInterstitial() {
-        AdRequest adRequest = new AdRequest.Builder()
-                .build();
-
-        mInterstitialAd.loadAd(adRequest);
-    }
-    // [END request_new_interstitial]
-
-    private void beginSecondActivity() { }
-
-    // [START add_lifecycle_methods]
-    /** Called when leaving the activity */
-    @Override
-    public void onPause() {
-        if (mAdView != null) {
-            mAdView.pause();
-        }
-        super.onPause();
-    }
-
-    /** Called when returning to the activity */
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (mAdView != null) {
-            mAdView.resume();
-        }
-        if (!mInterstitialAd.isLoaded()) {
-            requestNewInterstitial();
-        }
-    }
-
-    /** Called before the activity is destroyed */
-    @Override
-    public void onDestroy() {
-        if (mAdView != null) {
-            mAdView.destroy();
-        }
-        super.onDestroy();
-    }
-    // [END add_lifecycle_methods]
 
 }

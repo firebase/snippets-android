b57252bfeba8563e8499eebd07dd5d0a1f3c047e
Kevin Cheung <kevincheung@google.com>
2021-07-26 14:36:06 -0700

    Add App Check snippets

---
+++ appcheck/app/src/main/java/com/google/firebase/example/appcheck/kotlin/ApiWithAppCheckExample.kt
@@ -0,0 +1,34 @@
+package com.google.firebase.example.appcheck.kotlin
+
+import com.google.firebase.appcheck.FirebaseAppCheck
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.http.GET
+import retrofit2.http.Header
+
+
+// [START appcheck_custom_backend]
+class ApiWithAppCheckExample {
+    interface YourExampleBackendService {
+        @GET("yourExampleEndpoint")
+        fun exampleData(
+            @Header("X-Firebase-AppCheck") appCheckToken: String
+        ): Call<List<String>>
+    }
+
+    var yourExampleBackendService: YourExampleBackendService = Retrofit.Builder()
+        .baseUrl("https://yourbackend.example.com/yourApiEndpoint")
+        .build()
+        .create(YourExampleBackendService::class.java)
+
+    fun callApiExample() {
+        FirebaseAppCheck.getInstance()
+            .getAppCheckToken(false)
+            .addOnSuccessListener { tokenResponse ->
+                val appCheckToken = tokenResponse.token
+                val apiCall = yourExampleBackendService.exampleData(appCheckToken)
+                // ...
+            }
+    }
+}
+// [END appcheck_custom_backend]

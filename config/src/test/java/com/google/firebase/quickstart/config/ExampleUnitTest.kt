92f69cc46382f60d56c88d47a1c7869ab06d1d6e
rosariopf <rosariopf@google.com>
2022-05-28 17:37:38 +0100

    remove wildcard import from unitTest

--- config/src/test/java/com/google/firebase/quickstart/config/ExampleUnitTest.kt
+++ config/src/test/java/com/google/firebase/quickstart/config/ExampleUnitTest.kt
@@ -2,7 +2,7 @@ package com.google.firebase.quickstart.config
 
 import org.junit.Test
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 
 /**
  * Example local unit test, which will execute on the development machine (host).

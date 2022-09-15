3dc86f2b883970ac78b3a10496bbbf12ef14a925
Mark Arndt <markarndt@google.com>
2022-09-15 15:31:55 -0700

    Let's do this properly, analagously to RTDB snippets.

--- storage/app/src/main/java/com/google/firebase/referencecode/storage/EmulatorSuite.java
+++ storage/app/src/main/java/com/google/firebase/referencecode/storage/EmulatorSuite.java
@@ -6,7 +6,10 @@ public class EmulatorSuite {
 
     public void emulatorSettings() {
         // [START storage_emulator_connect]
-        FirebaseStorage.getInstance().useEmulator("10.0.2.2", 9199);
+        // 10.0.2.2 is the special IP address to connect to the 'localhost' of
+        // the host computer from an Android emulator.
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        storage.useEmulator("10.0.2.2", 9199);
         // [END storage_emulator_connect]
     }
 
--- storage/app/src/main/java/com/google/firebase/referencecode/storage/kotlin/EmulatorSuite.kt
+++ storage/app/src/main/java/com/google/firebase/referencecode/storage/kotlin/EmulatorSuite.kt
@@ -8,7 +8,10 @@ class EmulatorSuite {
 
     fun emulatorSettings() {
         // [START storage_emulator_connect]
-        Firebase.storage.useEmulator("10.0.2.2", 9199);
+        // 10.0.2.2 is the special IP address to connect to the 'localhost' of
+        // the host computer from an Android emulator.
+        val storage = Firebase.storage
+        storage.useEmulator("10.0.2.2", 9199);
         // [END storage_emulator_connect]
     }
 

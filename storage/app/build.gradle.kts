0c9a564001986dfe3366bfe33b5dfd8f78066603
Marina Coelho <marinacoelho16@gmail.com>
2023-05-15 13:55:53 +0100

    Use kts syntax in storage module

---
+++ storage/app/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("com.google.gms.google-services")
+    id("com.google.devtools.ksp") version "1.8.20-1.0.10"
+}
+
+android {
+    namespace = "com.google.firebase.referencecode.storage"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.google.firebase.referencecode.storage"
+        minSdk = 19
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+    lint {
+        // TODO(thatfiredev): Remove this once
+        //  https://github.com/bumptech/glide/issues/4940 is fixed
+        disable += "NotificationPermission"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.0.0"))
+
+    // Add the dependency for the Cloud Storage library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-storage-ktx")
+
+    implementation("com.firebaseui:firebase-ui-storage:8.0.2")
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+    ksp("com.github.bumptech.glide:ksp:4.15.1")
+
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+}

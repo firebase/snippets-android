d6ce44d11c178c03bf79998a7e4c12c3500dcd71
Marina Coelho <marinacoelho16@gmail.com>
2023-05-15 14:19:25 +0100

    Updated main gradle files to kts syntax

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+include(":auth:app",
+        ":appcheck:app",
+        ":config:app",
+        ":database:app",
+        ":dynamic-links:app",
+        ":dl-invites:app",
+        ":firebaseoptions:app",
+        ":functions:app",
+        ":firestore:app",
+        ":storage:app",
+        ":tasks:app",
+        ":inappmessaging:app",
+        ":admob:app",
+        ":messaging:app",
+        ":crashlytics:app",
+        ":perf:app",
+        ":test-lab:app",
+        ":analytics:app",
+        ":installations:app"
+)
+

1d15565e1bab356c76fe4f02b242c685a76c297f
Marina Coelho <marinacoelho16@gmail.com>
2023-05-15 13:12:14 +0100

    Use kts syntax in installations module

---
+++ installations/app/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.google.samples.snippet"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.google.samples.snippet"
+        minSdk = 19
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(fileTree("libs").include("*.jar"))
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation("com.google.firebase:firebase-installations:17.1.3")
+}

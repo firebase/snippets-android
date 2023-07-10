d24a6852465b629e9ea8bfb7a52cd6cbaa5329d8
DPEBot <dpebot@google.com>
2023-07-10 09:23:32 -0700

    Auto-update dependencies. (#469)
    * Auto-update dependencies.
    
    * refactor(database): replace forEach with for
    
    ---------
    
    Co-authored-by: rosariopf <rosariopf@google.com>

--- storage/app/build.gradle.kts
+++ storage/app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     id("com.google.gms.google-services")
-    id("com.google.devtools.ksp") version "1.8.22-1.0.11"
+    id("com.google.devtools.ksp") version "1.9.0-1.0.11"
 }
 
 android {

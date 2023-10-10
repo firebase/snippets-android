8bd4602acbef71ae6e18eb0d4248ff0a81b755f4
DPEBot <dpebot@google.com>
2023-10-10 02:59:15 -0700

    Auto-update dependencies. (#493)

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     id("com.google.firebase.crashlytics") version "2.9.9" apply false
     id("com.google.firebase.firebase-perf") version "1.4.2" apply false
     id("androidx.navigation.safeargs") version "2.7.4" apply false
-    id("com.github.ben-manes.versions") version "0.48.0" apply true
+    id("com.github.ben-manes.versions") version "0.49.0" apply true
 }
 
 allprojects {

124d413c5c1b88b30af740f7db9c8a695967dc6f
DPEBot <dpebot@google.com>
2024-03-05 08:44:13 -0800

    Auto-update dependencies. (#534)
    Brought to you by your friendly [Repository Gardener](https://togithub.com/GoogleCloudPlatform/repository-gardener).

--- auth/app/build.gradle.kts
+++ auth/app/build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     implementation("androidx.browser:browser:1.0.0")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.activity:activity-ktx:1.8.2")
 
     // Import the BoM for the Firebase platform

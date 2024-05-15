1f0f61bcbaf04412a0eb00c224bbf261d2d42ff9
DPEBot <dpebot@google.com>
2024-05-15 07:19:24 -0700

    Auto-update dependencies. (#550)
    * Auto-update dependencies.
    
    * compose compiler 1.5.14
    
    ---------
    
    Co-authored-by: Rosário P. Fernandes <rosariopf@google.com>

--- storage/app/build.gradle.kts
+++ storage/app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     id("com.google.gms.google-services")
-    id("com.google.devtools.ksp") version "1.9.23-1.0.20"
+    id("com.google.devtools.ksp") version "1.9.24-1.0.20"
 }
 
 android {

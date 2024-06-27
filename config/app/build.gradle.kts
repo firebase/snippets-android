0cfc4e2e1ade320fbf4e9d2384ce25ef7cb6691f
DPEBot <dpebot@google.com>
2024-06-27 05:12:15 -0500

    Auto-update dependencies. (#563)
    Brought to you by your friendly [Repository Gardener](https://togithub.com/GoogleCloudPlatform/repository-gardener).

--- config/app/build.gradle.kts
+++ config/app/build.gradle.kts
@@ -46,6 +46,6 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics")
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.0")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }

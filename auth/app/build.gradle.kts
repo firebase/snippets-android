44fe0e4f700b22a7913fc955bf0e8348bf46424a
DPEBot <dpebot@google.com>
2024-11-01 13:29:09 -0400

    Auto-update dependencies. (#592)

--- auth/app/build.gradle.kts
+++ auth/app/build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.browser:browser:1.0.0")
     implementation("androidx.cardview:cardview:1.0.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.activity:activity-ktx:1.9.3")
 

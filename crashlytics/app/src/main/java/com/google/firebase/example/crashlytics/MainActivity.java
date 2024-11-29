8cf4c69db87cce81b0620d6c5aef34957d51b943
Tejas Deshpande <tdeshpande@google.com>
2024-11-29 16:14:31 -0500

    Remove unused import

--- crashlytics/app/src/main/java/com/google/firebase/example/crashlytics/MainActivity.java
+++ crashlytics/app/src/main/java/com/google/firebase/example/crashlytics/MainActivity.java
@@ -8,7 +8,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.google.firebase.crashlytics.FirebaseCrashlytics;
-import com.google.firebase.crashlytics.FirebaseCrashlytics.*;
 
 public class MainActivity extends AppCompatActivity {
 

4d928a793ae1d45511206189c26c7b698c5c1b40
Rosário P. Fernandes <rosariofernandes51@gmail.com>
2025-02-28 13:15:34 +0000

    chore(admob): minSdk=23

--- admob/app/build.gradle.kts
+++ admob/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.google.firebase.example.admob"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"

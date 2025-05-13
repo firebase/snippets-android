44c1c6c466021b8b7f77eae1cd078983cf30a3f9
DPEBot <dpebot@google.com>
2025-05-13 09:00:29 -0700

    Auto-update dependencies. (#626)
    Brought to you by your friendly [Repository Gardener](https://togithub.com/GoogleCloudPlatform/repository-gardener).

--- storage/app/build.gradle.kts
+++ storage/app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     id("com.google.gms.google-services")
-    id("com.google.devtools.ksp") version "2.1.20-2.0.0"
+    id("com.google.devtools.ksp") version "2.1.20-2.0.1"
 }
 
 android {

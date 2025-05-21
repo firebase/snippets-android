78b8f43365a174aabd14593ecd56a319d75d9caa
DPEBot <dpebot@google.com>
2025-05-21 09:38:46 -0700

    Auto-update dependencies. (#628)

--- firebaseoptions/app/build.gradle.kts
+++ firebaseoptions/app/build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
 
     // Import the BoM for the Firebase platform
-    implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.14.0"))
     implementation("com.google.firebase:firebase-database")
     implementation("com.google.firebase:firebase-common")
 }

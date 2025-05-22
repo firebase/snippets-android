4b12ceb6d11ffc33adf16242ffe6d394e301dffa
rosariopf <rosariopf@google.com>
2025-05-22 19:08:56 +0100

    create GenerateText snippets

---
+++ firebase-ai/src/main/java/com/google/firebase/example/ai/send_requests/GenerateText.kt
@@ -0,0 +1,31 @@
+package com.google.firebase.example.ai.send_requests
+
+import com.google.firebase.ai.GenerativeModel
+
+class GenerateText {
+
+  private suspend fun textOnlyNonStreaming(model: GenerativeModel) {
+    // [START text_only_non_streaming]
+    // Provide a prompt that contains text
+    val prompt = "Write a story about a magic backpack."
+
+    // To generate text output, call generateContent with the text input
+    val response = model.generateContent(prompt)
+    print(response.text)
+    // [END text_only_non_streaming]
+  }
+
+  private suspend fun textOnlyStreaming(model: GenerativeModel) {
+    // [START text_only_streaming]
+    // Provide a prompt that includes only text
+    val prompt = "Write a story about a magic backpack."
+
+    // To stream generated text output, call generateContentStream and pass in the prompt
+    var response = ""
+    model.generateContentStream(prompt).collect { chunk ->
+      print(chunk.text)
+      response += chunk.text
+    }
+    // [END text_only_streaming]
+  }
+}

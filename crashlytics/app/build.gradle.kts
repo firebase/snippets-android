13e06772027dc34151efbb8e17dc793b04702ec4
Rosario Fernandes <rosariopf@google.com>
2025-12-19 14:48:25 +0000

    migrate from kotlinOptions {} to kotlin.compilerOptions {}

--- crashlytics/app/build.gradle.kts
+++ crashlytics/app/build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -28,8 +29,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.fromTarget("17")
+        }
     }
 }
 

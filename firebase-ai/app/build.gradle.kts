925ca50328c14aa3f34780ae3c92bfa226b0eaba
Rosario Fernandes <rosariopf@google.com>
2025-12-19 14:48:25 +0000

    migrate from kotlinOptions {} to kotlin.compilerOptions {}

--- firebase-ai/app/build.gradle.kts
+++ firebase-ai/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
@@ -34,8 +36,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.fromTarget("17")
+        }
     }
     buildFeatures {
         compose = true

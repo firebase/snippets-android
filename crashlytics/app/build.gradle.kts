6d0e3f7b7984f4cfd137f97f90445d0d5d469efd
DPEBot <dpebot@google.com>
2026-05-08 08:58:19 -0400

    Auto-update dependencies. (#679)

--- crashlytics/app/build.gradle.kts
+++ crashlytics/app/build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
 
     // Import the BoM for the Firebase platform
-    implementation(platform("com.google.firebase:firebase-bom:34.12.0"))
+    implementation(platform("com.google.firebase:firebase-bom:34.13.0"))
 
     // Add the dependencies for the Crashlytics and Analytics libraries
     // When using the BoM, you don't specify versions in Firebase library dependencies
